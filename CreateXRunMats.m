f3cc45e78d2b1ce5d375f92b6100f46aea552539
haimshalev <haimshalev@gmail.com>
2014-09-05 18:46:41 +0300

    Creating xRuns with half pos and half neg TRs

--- CreateXRunMats.m
+++ CreateXRunMats.m
@@ -35,8 +35,24 @@ end
 function subj = CreateNMinusOneXRuns(subj)
 
     % create leave one out runs
+    
+    % the method will randomly choose coupes of tr's with one neg and one
+    % pos condition. Thus creating n/2 cross validation groups
+    
+    [posRegs, negRegs, randPosRegsOrder, randNegRegsOrder] = SeperatePosAndNegTRs(subj, 'conds_sh3');
+    
     runs = get_mat(subj, 'selector', 'runs_xval_1');
-    runs(runs ~= 0) = 1 : count(runs);
+    
+    %set all the positive TRs groups
+    for i = 1 : length(randPosRegsOrder)
+        runs(posRegs(i)) = randPosRegsOrder(i);
+    end
+    
+    %and now for all the negative TRs
+    for i = 1 : length(randNegRegsOrder)
+        runs(negRegs(i)) = randNegRegsOrder(i);
+    end
+    
     subj = set_mat(subj, 'selector', 'runs', runs);
     
 end
@@ -45,22 +61,42 @@ function subj = CreateRandomPartitionsXRuns(subj)
     
     global globalVars;
     
+    [posRegs, negRegs, randPosRegsOrder, randNegRegsOrder] = SeperatePosAndNegTRs(subj, 'conds_sh3');
+    allRegs = length(posRegs) + length(negRegs);
+    
     % create a partition runs
     runs = get_mat(subj, 'selector', 'runs_xval_1');
-    randIndeces = randperm(count(runs));
-    partitionSize = ceil(count(runs)/globalVars.xRunPartitions);
+    partitionSizePos = ceil(count(runs)/(globalVars.xRunPartitions .* (allRegs/length(posRegs))));
+    partitionSizeNeg = ceil(count(runs)/(globalVars.xRunPartitions .* (allRegs/length(negRegs))));
     
+    % set the partitions for the positive TRs
+    currentPartition = 1;  
+    for i = 1 : length(posRegs)
+        runs(posRegs(randPosRegsOrder(i))) = currentPartition;
+        if mod(i,partitionSizePos) == 0
+            currentPartition = currentPartition + 1;
+        end
+    end
+   
+    % and set the partitions for the negative TRs
     currentPartition = 1;
-    randPartitions = zeros(size(randIndeces));
-    
-    for i = 1 : count(randIndeces)
-        randPartitions(randIndeces(i)) = currentPartition;
-        if mod(i,partitionSize) == 0
+    for i = 1 : length(negRegs)
+        runs(negRegs(randNegRegsOrder(i))) = currentPartition;
+        if mod(i,partitionSizeNeg) == 0
             currentPartition = currentPartition + 1;
         end
     end
     
-    runs(runs ~= 0) = randPartitions;
     subj = set_mat(subj, 'selector', 'runs', runs);
 
+end
+
+function [posTRs , negTRs, randPosRegsOrder, randNegRegsOrder] = SeperatePosAndNegTRs(subj, regressorName)
+
+    regs = get_mat(subj, 'regressors', regressorName);
+    posTRs = find(regs(1,:) ~= 0);
+    randPosRegsOrder = randperm(length(posTRs));
+    negTRs = find(regs(2,:) ~= 0);
+    randNegRegsOrder = randperm(length(negTRs));
+    
 end
